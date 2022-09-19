39778a001cb8facdf66d7671a33041ed8dad8ff2
okseorb <konstanstin@gmail.com>
2022-09-19 16:34:55 +0300

    Operations with Brauser Windows

--- src/main/java/ModalWindows.java
+++ src/main/java/ModalWindows.java
@@ -1,55 +1,65 @@
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Set;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
 public class ModalWindows {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","c:\\Users\\usr\\IdeaProjects\\test-selen\\drivers\\103\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
-        // Неявное ожидание
+        // Неявное ожидание (Implicit Wait)
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        // Явное ожидание (Explicit Wait)
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(1));
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
 
         try {
 
             driver.get("http://127.0.0.1:5500/index.html");
             Thread.sleep(5000);
 
+            //идентификатор текущего окна
+            String window1 = driver.getWindowHandle();
 
-            WebElement elem = driver.findElement(By.id("a"));
-            WebElement elem2 = driver.findElement(By.id("b"));
-            WebElement elem3 = driver.findElement(By.id("c"));
+            js.executeScript("window.open()");
 
-            elem.click();
+            String window2 = null;
 
-            Alert alert = wait.until(alertIsPresent());
-            alert.accept();
+            Set<String> currentSetWindows = driver.getWindowHandles();
 
-            elem2.click();
-            Alert promt = wait.until(alertIsPresent());
-            promt.sendKeys("I wrote it!");
-            promt.accept();
+            Integer i = 0;
+            for(String window : currentSetWindows){
+                i++;
+                if(!window.equals(window1)){
+                    window2 = window;
+                    break;
+                }
+            }
 
-            Alert alert2 = wait.until(alertIsPresent());
+            System.out.println("Через сколько окон нашли новое: " + i);
+
+            driver.switchTo().window(window2);
             Thread.sleep(2000);
-            alert2.accept();
+            driver.get("https://onliner.by");
+            Thread.sleep(2000);
 
-            elem3.click();
-            Alert alert3 = wait.until(alertIsPresent());
-            alert3.dismiss();
+            driver.switchTo().window(window1);
+            Thread.sleep(1000);
+
+            driver.switchTo().window(window2);
+            Thread.sleep(1000);
+            driver.close();
+            driver.switchTo().window(window1);
 
 
 
@@ -60,6 +70,7 @@ public class ModalWindows {
             e.printStackTrace();
 
         } finally {
+            Thread.sleep(2000);
             driver.quit();
         }
 
