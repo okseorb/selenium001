c16b66b352302b03b11c0ebee18fbb9de7492f15
okseorb <konstanstin@gmail.com>
2022-09-25 21:06:23 +0300

    TestNG Parameters and DataProvider

--- src/main/java/smokeTestNG/TestNG1.java
+++ src/main/java/smokeTestNG/TestNG1.java
@@ -1,12 +1,14 @@
 package smokeTestNG;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestNG1 {
 
+    @Parameters("db")
     @Test (groups ={"smokeTest", "regress"})
-    public void test1() {
-        System.out.println("Test 1");
+    public void test1(String db) {
+        System.out.println("Test 1, parametr is: " + db);
     }
     @Test (groups ={"regress"})
     public void test11() {
--- src/main/java/smokeTestNG/TestNG2.java
+++ src/main/java/smokeTestNG/TestNG2.java
@@ -1,16 +1,35 @@
 package smokeTestNG;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestNG2 {
 
-    @Test (groups ={"smokeTest", "regress"})
+    @Test(groups = {"smokeTest", "regress"})
     public void test2() {
         System.out.println("Test 2");
     }
 
-    @Test (groups ={"smokeTest"})
+    @Test(groups = {"smokeTest"})
     public void test22() {
         System.out.println("Test 22");
     }
+
+
+    @DataProvider(name = "dp")
+    public static Object[][] dbData() {
+        return new Object[][]{
+                {"SQL", new Integer(1)},
+                {"NOSQL", new Integer(2)}
+        };
+    }
+
+    @Parameters({"dp"})
+    @Test(dataProvider = "dp", groups = {"smokeTest", "regress"})
+    public void test222(String e1, int e2) {
+        System.out.println("Test 222 |" + " Параметр через DataProvider e1: " + e1 + " и e2: " + e2);
+    }
+
+
 }
